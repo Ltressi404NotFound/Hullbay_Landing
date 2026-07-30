09723039958c96744d4a6c4ecb3c676efd3025e2
MOUNIR-x <pam20072007@gmail.com>
2026-07-30 21:55:22 +0200

    (fix): correction of bug of Medusa ui

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@medusajs/ui/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 }
